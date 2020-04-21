2541c519928b49e843602d85ff5ffd32e8547d52
Edelweiss <barnwal.rishi@gmail.com>
2020-04-21 00:54:33 -0400

    Added: Skeleton of a controllable base script for all controllable gameobjects to inherit from

---
+++ Assets/Game/Scripts/Core/ControllableBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllableBase : MonoBehaviour
+{
+    public virtual void OnNorthFaceButtonDown() { }
+    public virtual void OnNorthFaceButton() { }
+    public virtual void OnNorthFaceButtonUp() { }
+    public virtual void OnEastFaceButtonDown() { }
+    public virtual void OnEastFaceButton() { }
+    public virtual void OnEastFaceButtonUp() { }
+    public virtual void OnSouthFaceButtonDown() { }
+    public virtual void OnSouthFaceButton() { }
+    public virtual void OnSouthFaceButtonUp() { }
+    public virtual void OnWestFaceButtonDown() { }
+    public virtual void OnWestFaceButton() { }
+    public virtual void OnWestFaceButtonUp() { }
+    public virtual void OnLeftShoulderButtonDown() { }
+    public virtual void OnLeftShoulderButton() { }
+    public virtual void OnLeftShoulderButtonUp() { }
+    public virtual void OnLeftTriggerButtonDown() { }
+    public virtual void OnLeftTriggerButton() { }
+    public virtual void OnLeftTriggerButtonUp() { }
+    public virtual void OnRightShoulderButtonDown() { }
+    public virtual void OnRightShoulderButton() { }
+    public virtual void OnRightShoulderButtonUp() { }
+    public virtual void OnRightTriggerButtonDown() { }
+    public virtual void OnRightTriggerButton() { }
+    public virtual void OnRightTriggerButtonUp() { }
+
+
+}
--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : Singleton<InputManager>
+public class PlayerController : ControllableBase
 {
-    public List<ControllableBase> controllableObjects;
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
