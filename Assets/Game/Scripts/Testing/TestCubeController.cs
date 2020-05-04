705254b6a6c39539b5637065aac3d10648bbfe62
Jesse Berube <berubeje@sheridancollege.ca>
2020-05-04 15:45:10 -0400

    Added: Installed Cinemachine, created a cube with basic movment and have a camera follow the cube.

---
+++ Assets/Game/Scripts/Testing/TestCubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCubeController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void FixedUpdate()
+    {
+         if (Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
