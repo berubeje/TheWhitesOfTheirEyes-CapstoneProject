a5cac7bbc6165a084cd708dd72af96aec30a78dd
Edelweiss <barnwal.rishi@gmail.com>
2020-05-08 00:45:01 -0400

    Added: Blending from walking to running, and animations for tilting when changing directions

--- Assets/Game/Scripts/Player/JimController.cs
+++ Assets/Game/Scripts/Player/JimController.cs
@@ -6,11 +6,16 @@ public class JimController : IPlayerControllable
 {
     public float rotationSpeed;
     public float turnAroundThreshold;
+    public float directionDampTime;
+    public float directionSpeed;
 
     private Animator _jimAnimator;
 
     private Vector2 _leftStickInput;
     private Vector2 _rightStickInput;
+
+    private Vector3 moveDirection;
+    private Vector3 leftStickDirection;
 
     private bool _isPivoting;
     void Start()
@@ -20,7 +25,6 @@ public class JimController : IPlayerControllable
 
     void Update()
     {
-        
     }
 
    
@@ -29,18 +33,34 @@ public class JimController : IPlayerControllable
         _leftStickInput.x = Input.GetAxis("Left Horizontal");
         _leftStickInput.y = Input.GetAxis("Left Vertical");
 
-        _jimAnimator.SetFloat("leftInputX", _leftStickInput.x);
-        _jimAnimator.SetFloat("leftInputY", _leftStickInput.y);
         _jimAnimator.SetFloat("leftInputMagnitude", _leftStickInput.magnitude, 0.15f, Time.deltaTime);
 
+        leftStickDirection = new Vector3(_leftStickInput.x, 0.0f, _leftStickInput.y);
+
+        // Get players forward and kill the y value
+        Vector3 playerDirection = transform.forward;
+        playerDirection.y = 0.0f;
+
+        // Create rotation from the players forward to the direction the joystick is being held
+        Quaternion referenceShift = Quaternion.FromToRotation(playerDirection, leftStickDirection);
+
+        // Convert joystick input to world space
+        moveDirection = referenceShift * leftStickDirection;
+
+        // y value of this vector is used to figure out if the direction is left or right of the player
+        Vector3 axisSign = Vector3.Cross(moveDirection, transform.forward);
+
+        float direction = Vector3.Angle(transform.forward, moveDirection) * (axisSign.y > 0 ? -1.0f : 1.0f);
+        direction /= 180.0f;
+        direction *= directionSpeed;
+
+        _jimAnimator.SetFloat("direction", direction, directionDampTime, Time.deltaTime);
+
+        // Directly rotate the player if the joystick is moving 
         if (_leftStickInput != Vector2.zero)
         {
-            Vector3 leftStickInputVector3 = new Vector3(_leftStickInput.x, 0.0f, _leftStickInput.y);
-            Quaternion targetRotation = Quaternion.LookRotation(leftStickInputVector3);
-            float angle = Quaternion.Angle(targetRotation, transform.rotation);
+            Quaternion targetRotation = Quaternion.LookRotation(leftStickDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
-            
-            
         }
     }
 
@@ -48,5 +68,12 @@ public class JimController : IPlayerControllable
     {
         _rightStickInput.x = Input.GetAxis("Right Horizontal");
         _rightStickInput.y = Input.GetAxis("Right Vertical");
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z), moveDirection, Color.red);
+        Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z), leftStickDirection, Color.green);
     }
 }
