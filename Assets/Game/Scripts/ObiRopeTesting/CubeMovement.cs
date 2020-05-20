6f7f361823d672f6f269c44e892bf065f9c34e00
Jesse Berube <berubeje@sheridancollege.ca>
2020-05-20 13:28:55 -0400

    Added: Added swinging and pulling of the block using the rope. Needs more work as the pyshics are a bit wonky

---
+++ Assets/Game/Scripts/ObiRopeTesting/CubeMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float acceleration = 10;
+    public float maxSpeed = 8;
+    public float jumpPower = 2;
+
+    private Rigidbody unityRigidbody;
+    private GrapplingHookType1 grapplingHook1;
+    private GrapplingHookType2 grapplingHook2;
+    public bool onGround = false;
+    public bool hookOut = false;
+    public bool hookLaunched = false;
+
+    public void Awake()
+    {
+        grapplingHook1 = GetComponentInChildren<GrapplingHookType1>();
+        grapplingHook2 = GetComponentInChildren<GrapplingHookType2>();
+
+        unityRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+   
+        if(grapplingHook1 != null)
+        {
+            GrapplingHookType1 grapplingHook = grapplingHook1;
+            hookOut = grapplingHook.hookOut;
+    
+        }
+        else if(grapplingHook2 != null)
+        {
+            GrapplingHookType2 grapplingHook = grapplingHook2;
+            hookOut = grapplingHook.hookOut;
+            hookLaunched = grapplingHook.launched;
+        }
+
+        if(hookLaunched == true)
+        {
+            unityRigidbody.isKinematic = true;
+        }
+        else
+        {
+            unityRigidbody.isKinematic = false;
+        }
+
+        if (!onGround && !hookOut)
+        {
+            return;
+        }
+        else if(hookOut)
+        {
+            unityRigidbody.AddForce(new Vector3(0, 0, Input.GetAxis("Vertical") * acceleration), ForceMode.Acceleration);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                unityRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            }
+        }
+        else
+        {
+            unityRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * acceleration, 0, 0), ForceMode.Acceleration);
+            unityRigidbody.AddForce(new Vector3(0, 0, Input.GetAxis("Vertical") * acceleration), ForceMode.Acceleration);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                unityRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            }
+        }
+
+        unityRigidbody.velocity = Vector3.ClampMagnitude(unityRigidbody.velocity, maxSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/ObiRopeTesting/MagicRopeProjectileLogic.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicRopeProjectileLogic : MonoBehaviour
+{
+    public float projectileSpeed;
+    public GrapplingHookType1 grapplingHookLogic1;
+    public GrapplingHookType2 grapplingHookLogic2;
+
+    private Vector3 targetPosition;
+    private GameObject targetGameObject;
+    private bool targetReached = false;
+    private Rigidbody rigidBody;
+
+
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        if (targetReached == false)
+        {
+            float step = projectileSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
+            {
+                if (grapplingHookLogic1 != null)
+                {
+                    grapplingHookLogic1.TargetReached();
+                    targetReached = true;
+                    
+                    if(targetGameObject.GetComponent<BoxScript>() != null)
+                    {
+                       // rigidBody.isKinematic = false;
+                        transform.parent = targetGameObject.transform;
+                    }
+
+                }
+                else
+                {
+                    grapplingHookLogic2.TargetReached();
+                    targetReached = true;
+
+                    if (targetGameObject.GetComponent<BoxScript>() != null)
+                    {
+                       // rigidBody.isKinematic = false;
+                        transform.parent = targetGameObject.transform;
+
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetupProjectile(Vector3 position, GrapplingHookType1 grappling, GameObject target)
+    {
+        targetPosition = position;
+        grapplingHookLogic1 = grappling;
+        targetGameObject = target;
+    }
+
+    public void SetupProjectile(Vector3 position, GrapplingHookType2 grappling, GameObject target)
+    {
+        targetPosition = position;
+        grapplingHookLogic2 = grappling;
+        targetGameObject = target;
+
+    }
+}
