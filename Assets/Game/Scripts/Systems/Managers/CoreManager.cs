d763efbe402e7e9bc11c671cfc4522e573c20edf
Edelweiss <barnwal.rishi@gmail.com>
2020-06-22 18:34:35 -0400

    Added: Core manager script

---
+++ Assets/Game/Scripts/Systems/Managers/CoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreManager : MonoBehaviour
+{
+    private static CoreManager _instance;
+    private void Awake()
+    {
+        //DontDestroyOnLoad(this);
+
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
