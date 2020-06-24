01ec0b4612127f61c708a6cc4b159a397f77f3d2
Jesse Berube <berubeje@sheridancollege.ca>
2020-06-24 12:38:06 -0400

    Added: New camera dolly transition system

---
+++ Assets/Game/Scripts/Systems/DollyCameraManager.cs
@@ -0,0 +1,24 @@
+﻿///-------------------------------------------------------------------------------------------------
+// file: DollyCameraManager.cs
+//
+// author: Jesse Berube
+// date: 06/24/2020
+//
+// summary: Keeps track of the current dolly track, as well as switch tracks
+///-------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyCameraManager : Singleton<DollyCameraManager>
+{
+    public GameObject currentDolly;
+
+    public void ChangeDolly(GameObject newDolly)
+    {
+        currentDolly.SetActive(false);
+        newDolly.SetActive(true);
+        currentDolly = newDolly;
+    }
+}
