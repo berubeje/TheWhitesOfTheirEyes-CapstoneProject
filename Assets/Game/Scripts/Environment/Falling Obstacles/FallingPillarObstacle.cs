6b743f4ca94884501acc330c6f25dfe44ba85837
Edelweiss <barnwal.rishi@gmail.com>
2020-07-20 17:28:15 -0400

    Added: Animation events for footsteps and rolling

---
+++ Assets/Game/Scripts/Environment/Falling Obstacles/FallingPillarObstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPillarObstacle : IObstacle
+{
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
+
+    private RopeAnchorPoint _anchorPoint;
+    private MeshRenderer _anchorPointMeshRenderer;
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
+
+        _anchorPoint = GetComponentInChildren<RopeAnchorPoint>();
+        _anchorPointMeshRenderer = _anchorPoint.GetComponent<MeshRenderer>();
+    }
+    public override void ResetObstacle()
+    {
+        transform.position = _initialPosition;
+        transform.rotation = _initialRotation;
+        _anchorPoint.cantAttach = false;
+        _anchorPointMeshRenderer.enabled = true;
+        isTriggered = false;
+    }
+
+    public override void UnresetObstacle()
+    {
+    }
+}
