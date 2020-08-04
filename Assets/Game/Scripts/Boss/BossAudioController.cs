977971906eafd5091d45ce5fddf752cb291774a8
Edelweiss <barnwal.rishi@gmail.com>
2020-08-04 16:59:17 -0400

    Added: Boss step sound effect and boss audio controller

---
+++ Assets/Game/Scripts/Boss/BossAudioController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAudioController : MonoBehaviour
+{
+    public AudioClip[] stepSounds;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void StepEvent()
+    {
+        // Choose a random step sound and play it
+        int index = UnityEngine.Random.Range(0, stepSounds.Length);
+        _audioSource.clip = stepSounds[index];
+
+        _audioSource.Play();
+    }
+}
