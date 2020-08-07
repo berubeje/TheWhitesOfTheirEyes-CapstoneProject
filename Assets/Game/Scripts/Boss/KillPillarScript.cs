2e8deb8a63cf6d1b14d69e2fcace43f5c50cfeb0
Jesse Berube <berubeje@sheridancollege.ca>
2020-08-07 11:46:09 -0400

    Add: Kill pilars now spawn rubble of itself when it hits the boss

--- Assets/Game/Scripts/Boss/KillPillarScript.cs
+++ Assets/Game/Scripts/Boss/KillPillarScript.cs
@@ -16,14 +16,38 @@ public class KillPillarScript : MonoBehaviour
     public GameObject crumblingPillarPrefab;
     public float damageDelt = 25.0f;
 
+    private RopeAnchorPoint _anchorPoint;
+    
+
+    private void Awake()
+    {
+        _anchorPoint = transform.parent.GetComponentInChildren<RopeAnchorPoint>();
+
+        _anchorPoint.pullStartEvent.AddListener(PullStarted);
+    }
+
+
+    private void PullStarted()
+    {
+        Collider playerCollider = InputManager.Instance.jimController.GetComponent<Collider>();
+
+        Physics.IgnoreCollision(playerCollider, GetComponent<Collider>(), true);
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.GetComponent<GiantDeathHitboxLogic>() != null)
         {
-            Transform copiedTranform = this.transform;
+            Transform copiedTranform = this.transform.parent;
 
             Instantiate(crumblingPillarPrefab, copiedTranform.position, copiedTranform.rotation);
             Destroy(this.gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        _anchorPoint.pullStartEvent.RemoveListener(PullStarted);
     }
 }
