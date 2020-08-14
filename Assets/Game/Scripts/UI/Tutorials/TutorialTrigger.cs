e00263be0771c324b85e0edd6d9bbb00e7ba41b7
Edelweiss <barnwal.rishi@gmail.com>
2020-08-13 21:54:20 -0400

    Added: Fungus trigger for first tutorial

---
+++ Assets/Game/Scripts/UI/Tutorials/TutorialTrigger.cs
@@ -0,0 +1,19 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("SFLHDF");
+            flowchart.gameObject.SetActive(true);
+            Destroy(this);
+        }
+    }
+}
